65fd22fcf6f6a2a2e8122e46f70532dabba63a31
Mehrdad Mizani <mehrdadmizani@gmail.com>
2022-12-13 03:36:55 +0000

    Broadcast added, Pythonic code added

--- TRE/Quan_logic_copy.py
+++ TRE/Quan_logic_copy.py
@@ -3,12 +3,10 @@ from pyspark.sql import SparkSession
 
 spark_pyspark = SparkSession.builder.master(
     "local[1]").appName("cci").getOrCreate()
-
-# columns = ["PERSON_ID", "cov_hx_out_mi_flag", "cov_hx_out_mi_date", "cov_hx_out_ckd_flag", "cov_hx_out_ckd_date",
-#        "cov_hx_out_dm_flag", "cov_hx_out_dm_date", "cov_hx_out_chd_flag", "cov_hx_out_chd_date"]
+#sc = SparkContext('local', 'ccisc')
 
 columns = ["patid", "mi", "chf", "pvd", "cevd", "dementia", "cpd", "rheumd", "pud", "diab", "diabwc", "hp", "rend",
-           "mld", "msld", "canc", "metacanc", "aids", "expected_all", "expected_mutual_exclusive_"]
+           "mld", "msld", "canc", "metacanc", "aids", "expected_all", "expected_mutual_exclusive"]
 data = [
     ("cat_1", "0", "0", "0", "0", "0", "0", "0", "0", "0",
      "0", "0", "0", "0", "0", "1", "0", "1", "6", "6"),
@@ -25,11 +23,11 @@ data = [
     ("cat_31", "0", "0", "0", "0", "0", "0", "0", "0", "1",
      "1", "0", "0", "1", "0", "0", "1", "0", "9", "9"),
     ("cat_4", "0", "0", "0", "0", "0", "0", "0", "0", "0",
-        "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"),
+     "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"),
     ("cat_5", "1", "0", "1", "1", "0", "0", "0", "1", "1",
-        "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"),
+     "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"),
     ("cat_6", "0", "0", "0", "0", "0", "0", "0", "0", "0",
-        "0", "0", "0", "0", "0", "0", "0", "1", "4", "4"),
+     "0", "0", "0", "0", "0", "0", "0", "1", "4", "4"),
 ]
 
 df = spark_pyspark.createDataFrame(data).toDF(*columns)
@@ -53,36 +51,36 @@ map_all = {
     "canc": 2,
     "mld": 2
 }
-
+map_all_bc = spark_pyspark.sparkContext.broadcast(map_all)
 map_mutual_exclusive = {
     "diabwc": "diab",
     "metacanc": "canc",
     "msld": "mld"
 }
+map_mutual_exclusive_bc = spark_pyspark.sparkContext.broadcast(map_mutual_exclusive)
 # Add columns to store weights
-col_list = map_all.keys()
+col_list = map_all_bc.value.keys()
 prefix = "quan"
 new_col_list = ["_".join([prefix, x]) for x in col_list]
 for col, new_col in zip(col_list, new_col_list):
-
     df = df.withColumn(new_col, F.when(
-        F.col(col) == 1, F.lit(map_all.get(col))).otherwise(F.lit(0)))
+        F.col(col) == 1, F.lit(map_all_bc.value.get(col))).otherwise(F.lit(0)))
 
 # Add columns for mild conditions of mutually-exclusive comorbidity pairs
-prefixed_mild_cols = ["_".join([prefix, x]) for x in map_mutual_exclusive.values()]
-new_col_list_mex = [
-    item for item in new_col_list if item not in prefixed_mild_cols]
+prefixed_mild_cols = ["_".join([prefix, x]) for x in map_mutual_exclusive_bc.value.values()]
 
-for key in map_mutual_exclusive.keys():
-    weighted_mex_col = "_".join(["mex", prefix, map_mutual_exclusive.get(key)])
+new_col_list_mex = list(filter(lambda x: x not in prefixed_mild_cols, new_col_list))
+for key in map_mutual_exclusive_bc.value.keys():
+    weighted_mex_col = "_".join(["mex", prefix, map_mutual_exclusive_bc.value.get(key)])
     new_col_list_mex.append(weighted_mex_col)
     df = df.withColumn(weighted_mex_col, F.when(F.col(key) != 0, F.lit(
-        0)).otherwise(F.col("_".join([prefix, map_mutual_exclusive.get(key)]))))
+        0)).otherwise(F.col("_".join([prefix, map_mutual_exclusive_bc.value.get(key)]))))
 
 # Calculate Quan CCI
+df = df.withColumn("Experimental_quan_all", F.expr('+'.join(new_col_list)))
 df = df.withColumn("quan_mutual_exclusive", F.expr('+'.join(new_col_list_mex)))
-df = df.withColumn("_DO_NOT_USE_quan_all", F.expr('+'.join(new_col_list)))
 
 df = df.drop(*new_col_list)
 df = df.drop(*new_col_list_mex)
 df.show()
+print(df.explain(extended=True))
